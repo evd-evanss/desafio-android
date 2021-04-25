e8801e735d29e0640570c5bc2d2a5f566eabfe6e
Evandro <revandro77@yahoo.com.br>
2021-04-25 15:52:33 -0300

    data caching completed

--- app/src/main/java/com/picpay/desafio/android/data/source/UserDataSource.kt
+++ app/src/main/java/com/picpay/desafio/android/data/source/UserDataSource.kt
@@ -3,14 +3,16 @@ package com.picpay.desafio.android.data.source
 import com.picpay.desafio.android.domain.model.remote.UserResponse
 import com.picpay.desafio.android.utils.factory.RetrofitServiceFactory
 import kotlinx.coroutines.flow.flow
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import javax.inject.Inject
 
 class UserDataSource @Inject constructor(
-    private val retrofit: Retrofit.Builder
+    private val retrofit: Retrofit.Builder,
+    private val okHttpClient: OkHttpClient
 ) {
-    private val service = RetrofitServiceFactory(retrofit)
+    private val service = RetrofitServiceFactory(retrofit, okHttpClient)
         .newInstance<UserService>()
 
     fun fetchUsers() = flow {
--- app/src/main/java/com/picpay/desafio/android/di/modules/RetrofitModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/modules/RetrofitModule.kt
@@ -2,12 +2,18 @@ package com.picpay.desafio.android.di.modules
 
 import android.content.Context
 import com.picpay.desafio.android.BuildConfig
+import com.picpay.desafio.android.utils.Constants.CACHE_HEADER
+import com.picpay.desafio.android.utils.Constants.FIVE_MEGA
+import com.picpay.desafio.android.utils.Constants.ONE_MINUTE
+import com.picpay.desafio.android.utils.Constants.ONE_WEEK
+import com.picpay.desafio.android.utils.extensions.hasNetwork
 import com.readystatesoftware.chuck.ChuckInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,12 +25,24 @@ object RetrofitModule {
 
     @Provides
     @Singleton
-    fun retrofit(@ApplicationContext context: Context): Retrofit.Builder {
-        val httpClient = OkHttpClient.Builder()
-        httpClient.addInterceptor(ChuckInterceptor(context))
+    fun retrofit(): Retrofit.Builder {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .client(httpClient.build())
             .addConverterFactory(GsonConverterFactory.create())
     }
+
+    @Provides
+    @Singleton
+    fun providesClient(@ApplicationContext context: Context) = OkHttpClient.Builder()
+        .cache(Cache(context.cacheDir, (FIVE_MEGA).toLong()))
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (context.hasNetwork())
+                request.newBuilder().header(CACHE_HEADER, ONE_MINUTE).build()
+            else
+                request.newBuilder().header(CACHE_HEADER, ONE_WEEK).build()
+            chain.proceed(request)
+        }
+        .addInterceptor(ChuckInterceptor(context))
+        .build()
 }
